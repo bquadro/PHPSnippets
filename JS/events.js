2f4c2debdf5fa91a2ed327fa1fbb37805b311547
Bquadro <support@bquadro.ru>
2015-11-13 15:12:08 +0300

    events js add

---
+++ JS/events.js
@@ -0,0 +1,12 @@
+(function($){
+
+  $( document ).ready(function() {
+      console.log( "ready!" );
+  });
+  
+  // Shorthand for $( document ).ready()
+  $(function() {
+      console.log( "ready!" );
+  });  
+
+})(jQuery);
