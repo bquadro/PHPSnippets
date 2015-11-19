87ace6c5fa6c173fab24283b63776697cff640ab
Bquadro <support@bquadro.ru>
2015-11-19 15:56:08 +0300

    1

--- JS/sendEvent.js
+++ JS/sendEvent.js
@@ -26,3 +26,34 @@ function sendEvent(event, category, virtual) {
 		}
 		return true;
 }
+
+//email select event
+
+if(!window.Kolich){
+  Kolich = {};
+}
+
+Kolich.Selector = {};
+Kolich.Selector.getSelected = function(){
+  var t = '';
+  if(window.getSelection){
+    t = window.getSelection();
+  }else if(document.getSelection){
+    t = document.getSelection();
+  }else if(document.selection){
+    t = document.selection.createRange().text;
+  }
+  return t;
+}
+
+Kolich.Selector.mouseup = function(){
+  var st = Kolich.Selector.getSelected();
+  if(st!=''){
+	var re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(re.test(st)){
+      sendEvent('click-copy', 'e-mail');
+    }
+  }
+}
+
+$(document).bind("mouseup", Kolich.Selector.mouseup);
